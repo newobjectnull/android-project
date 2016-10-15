757aea4b342e14d2047290679c472713eed99063
newobjectnull <new.object@null.net>
2016-10-15 18:07:59 +0200

    caricamento progetto

---
+++ app/src/main/java/com/example/dfontana/prova/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.dfontana.prova;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.testoCiao);
+    }
+
+    protected void cambiaTesto(View view){
+        textView.setText("Ha Ha funziona");
+    }
+}
